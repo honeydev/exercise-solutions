d30702a146b17e3f1e07b3f6009ed92f171eda6c
honey <devspades@gmail.com>
2017-03-29 15:06:50 +1000

    add Roman nums

---
+++ LeetCode and other/algorithms/Roman to Integer.js
@@ -0,0 +1,52 @@
+/**
+Roman to Integer
+Given a roman numeral, convert it to an integer.
+Input is guaranteed to be within the range from 1 to 3999.
+https://leetcode.com/problems/roman-to-integer/#/description
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(string) {
+    var pattern = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    }
+    var arabianArr = (function(s) {
+      romanArr = string.split('');
+      numArr = [];
+      console.log(romanArr);
+      for (var j = 0; j < romanArr.length; j++) {
+        if (pattern[romanArr[j]] !== undefined) {
+          numArr.push(pattern[romanArr[j]]);
+        }
+      }
+      return numArr;
+    })();
+    var result = (function() {
+      var result = 0;
+      for (var j = 0; j < arabianArr.length; j++) {
+        if (arabianArr[j+1] > arabianArr[j]) {
+          result += arabianArr[j+1] - arabianArr[j];
+          j++;
+          continue;
+        }
+        result += arabianArr[j];
+	  }
+      return result;
+    })();
+    return result;
+};
+
+//"DCXXI" = 500 100 10 10 1 = 621
+//"DCXIX" = 500 100 10 1 10 9 = 619
+//если число справа a больше номиналом текущего числа n следовательно это одно чилсо равное a - n плюсуем к результату, инкремент к счетичку, пропускаем итерацию
+//если нет просто плюсуем
+//возвращаем получившуюся сумму массива
