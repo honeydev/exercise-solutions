8ef00741a7f7d3d2d7ddf0673ac41ea8cf8cac45
honey <devspades@gmail.com>
2018-04-23 13:59:07 +1000

    fix 1.17

--- sicp/1.17.scm
+++ sicp/1.17.scm
@@ -4,5 +4,5 @@
 (define (halve n) (/ n 2))
 (define (mult a b)
   (cond ((= b 0) 0)
-        ((even? b) (mult (double a) (halve b)))
+        ((even? b) (double(mult (halve b))))
         (else (+ a (mult a (- b 1))))))
