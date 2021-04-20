20f60d133eebfff1daa41194b93df49633a8ba45
a.s.lebedev <a.s.lebedev@fun-box.ru>
2021-04-21 03:29:21 +1000

    update structure, add solutions

--- sicp/1.17.scm
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-(define (double n) (* n 2))
-(define (halve n) (/ n 2))
-(define (mult a b)
-  (cond ((= b 0) 0)
-        ((even? b) (double(mult (halve b))))
-        (else (+ a (mult a (- b 1))))))
